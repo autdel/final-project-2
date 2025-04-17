c5010a20e8354ddece6ed918cf113c7588ad8ba7
autdel <autdel@byu.edu>
2025-04-16 23:48:02 -0600

    minor fix

--- language-learning-game/src/components/state/game/GameContext.tsx
+++ language-learning-game/src/components/state/game/GameContext.tsx
@@ -1,14 +1,12 @@
 // src/contexts/game/GameContext.tsx
 import { createContext } from 'react';
-import { GameContextType, VocabularyParams } from './gameTypes';
+import { GameContextType } from './gameTypes';
 import { initialState } from './gameReducer';
 
 const defaultContextValue: GameContextType = {
   state: initialState,
   dispatch: () => null,
-  // NOTE: I need to revisit this to see if theres a fix for this thing
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  loadVocabulary: async (overrideParams?: VocabularyParams) => {},
+  loadVocabulary: async () => {},
   startCountdown: () => {},
   startGame: () => {},
   endGame: () => {},
